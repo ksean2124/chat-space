adbac02b59c7fc2bfb3b8eee26b38b76da2e3761
ksean2124 <60636102+ksean2124@users.noreply.github.com>
2020-02-18 19:24:34 +0900

    AC2

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -50,7 +50,6 @@ $('#new_message').on('submit', function(e){
       contentType: false
     })
      .done(function(data){
-       console.log(data);
        var html = buildHTML(data);
        $('.chat-main__message-list').append(html);
        $('.chat-main__message-list').animate({ scrollTop: $('.chat-main__message-list')[0].scrollHeight});
